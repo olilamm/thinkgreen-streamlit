b3367711aa6b755bbe3ffea3629afc7cecd60047
olilamm <olamm@vols.utk.edu>
2023-05-04 20:10:30 -0400

    urls

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import leafmap.foliumap as leafmap
 st.set_page_config(layout="wide")
 
 markdown = """
-Web App URL: <https://geotemplate.streamlit.app>
-GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
+Web App URL: <https://olilamm-thinkgreen.streamlit.app/>
+GitHub Repository: <https://github.com/olilamm/thinkgreen-streamlit.git>
 """
 
 st.sidebar.title("About")
